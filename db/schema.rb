34ca4b7e699f2d72bee828ca7e8b244b5ae67bd5
ssalaniponi <steven_salaniponi@yahoo.co.uk>
2016-01-13 17:32:14 +0200

    Contest Functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113082117) do
+ActiveRecord::Schema.define(version: 20160113150231) do
 
   create_table "accounts", force: true do |t|
     t.string   "shopify_account_url"
@@ -21,6 +21,19 @@ ActiveRecord::Schema.define(version: 20160113082117) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "contests", force: true do |t|
+    t.string   "name"
+    t.integer  "product_id"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.integer  "max_results"
+    t.integer  "order_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "contests", ["order_id"], name: "index_contests_on_order_id"
 
   create_table "order_items", force: true do |t|
     t.integer  "order_id"
---
+++ spec/factories/contests.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :contest do
+    name "MyString"
+product_id 1
+start_date "2016-01-13 17:02:31"
+end_date "2016-01-13 17:02:31"
+max_results 1
+order_id 1
+  end
+
+end
